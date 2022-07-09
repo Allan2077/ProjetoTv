e9a6e58abc133da3dc1a2beea61ff06f09521fe8
Allan2077 <allan.cesar@hotmail.com>
2022-07-08 22:28:25 -0300

    Atualizando

--- ProjetoTV/Home/home.js
+++ ProjetoTV/Home/home.js
@@ -1,15 +1,18 @@
 
 const container = document.querySelector('.container')
-var ButtonOn = document.getElementById('ButtonOn')
+const on = document.querySelector('.on')
 
-/*ButtonOn.addEventListener('click', function colorButton() {
+/*var ButtonOn = document.getElementById('ButtonOn')
+ButtonOn.addEventListener('click', function colorButton() {
     ButtonOn.style.backgroundColor = 'rgb(255, 85, 241)';
-
-
 });
 */
 
 function darkMode() {
     container.classList.toggle('darkMode');
     
+}
+
+function cleanLight(){
+    on.classList.toggle('cleanLight')
 }
--- ProjetoTvComCorpo/home.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector('.container')
-
-
-
-function modeChange(){
-    container.classList.toggle('modeChange');
-}
